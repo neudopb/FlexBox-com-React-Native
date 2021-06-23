60a21231b26e0d8b8686631e2566b4d082f2da61
neudinho11 <neudinho66@gmail.com>
2021-06-23 16:25:41 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -5,8 +5,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={ styles.box }>
+        <Text style={ styles.numero }>1</Text>
+      </View>
+      <View style={ styles.box }>
+        <Text style={ styles.numero }>2</Text>
+      </View>
+      <View style={ styles.box }>
+        <Text style={ styles.numero }>3</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +21,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#f7ffe4',
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'flex-start'
+  },
+  box: {
+    width: 100,
+    height: 100,
+    margin: 10,
+    backgroundColor: '#A9BCD0',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  numero: {
+    fontSize: 25
   },
 });
