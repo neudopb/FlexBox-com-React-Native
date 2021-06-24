0cb88415a83648deff21299376cd78587beff7ef
neudinho11 <neudinho66@gmail.com>
2021-06-24 10:42:36 -0300

    Versão final

--- App.js
+++ App.js
@@ -5,14 +5,50 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={ styles.box }>
-        <Text style={ styles.numero }>1</Text>
+      <View style={styles.menu}>
+        <View style={styles.boxMenu}>
+          <Text style={styles.txtMenu}>Menu</Text>
+        </View>
+        <View style={styles.boxMenu}>
+          <Text style={styles.txtMenu}>Logs</Text>
+        </View>
+        <View style={styles.boxMenu}>
+          <Text style={styles.txtMenu}>Credits</Text>
+        </View>
       </View>
-      <View style={ styles.box }>
-        <Text style={ styles.numero }>2</Text>
+
+      <View style={styles.corpo}>
+        <View style={styles.boxCorpo1}>
+          <View style={styles.cx1}></View>
+          <View style={styles.cx1}></View>
+          <View style={styles.cx1}></View>
+        </View>
+        <View style={styles.boxCorpo2}>
+          <View style={styles.cx2}></View>
+          <View style={styles.cx2}></View>
+          <View style={styles.cx2}></View>
+        </View>
+        <View style={styles.boxCorpo3}>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+          <View style={styles.cx3}></View>
+        </View>
       </View>
-      <View style={ styles.box }>
-        <Text style={ styles.numero }>3</Text>
+      
+      <View style={styles.rodape}>
+        <Text style={styles.txtRodape}>Exercício 01 - DDM</Text>
       </View>
     </View>
   );
@@ -21,20 +57,92 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f7ffe4',
+    backgroundColor: '#E5E5E5',
+    flexDirection: 'column',
+  },
+
+  menu: {
+    width: '100%',
+    height: 56,
+    marginTop: 29,
+    backgroundColor: '#C4C4C4',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  boxMenu: {
+    width: 77,
+    height: 28,
+    backgroundColor: '#000000',
+    borderRadius: 10,
+    margin: 11,
+    alignItems: 'center',
+  },
+  txtMenu: {
+    color: '#fff',
+    fontSize: 20,
+  },
+
+  corpo: {
+    flex: 1,
+    backgroundColor: '#B2B9FF',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    margin: 55,
+    borderRadius: 20,
+  },
+  boxCorpo1: {
+    width: 230,
+    height: 124,
+    backgroundColor: '#E5E5E5',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  boxCorpo2: {
+    width: 230,
+    height: 124,
+    backgroundColor: '#E5E5E5',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  boxCorpo3: {
+    width: 230,
+    height: 124,
+    backgroundColor: '#E5E5E5',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+  },
+  cx1: {
+    backgroundColor: '#000',
+    width: 45,
+    height: 106,
+    margin: 10,
+  },
+  cx2: {
+    width: 209,
+    height: 31,
+    backgroundColor: '#000',
+    margin: 10,
+  },
+  cx3: {
+    width: 35,
+    height: 31,
+    backgroundColor: '#000',
+    margin: 5,
+  },
+
+  rodape: {
+    width: '100%',
+    height: 55,
+    backgroundColor: '#4E466A',
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'flex-start'
-  },
-  box: {
-    width: 100,
-    height: 100,
-    margin: 10,
-    backgroundColor: '#A9BCD0',
     alignItems: 'center',
-    justifyContent: 'center'
   },
-  numero: {
-    fontSize: 25
-  },
+  txtRodape: {
+    color: '#fff',
+    fontSize: 20,
+  }
+  
 });
